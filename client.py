fd1014f7491136379027a4e0fa42502baf6f0831
Ric Janus Sapasap <ric.sapasap@geant.org>
2025-08-11 00:23:56 +0200

    Luh?

--- client.py
+++ client.py
@@ -4,21 +4,35 @@
 # ///
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class User(BaseModel):
     name: str | None = None
 
 @app.get("/user")
 async def get_user() -> User:
-    return User(name=None)
+    return User(name="James")
 
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(
-        "client:app",                      # use the actual object
+        "client:app",
         host="127.0.0.1",
         port=56789,
         reload=True,
