e7548f6f26477ddd5370285dc6b078216bb7c9b9
Ric Janus Sapasap <ric.sapasap@geant.org>
2025-08-12 15:32:19 +0200

    galing naman 5

--- src/client.py
+++ src/client.py
@@ -1,83 +1,47 @@
 #!/usr/bin/env -S uv run --script
 # /// script
-# dependencies = ["fastapi", "uvicorn", "sqlmodel"]
+# dependencies = ["fastapi", "uvicorn", "pyyaml"]
 # ///
 from contextlib import asynccontextmanager
-from pathlib import Path
+from typing import Any
 
+import yaml
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import getpass
-
-from sqlmodel import SQLModel, create_engine, Session, select
-
-from models import Ejeweje
-
-# - DB config
-DB_PATH = Path("data/self.db")
-DB_URL = f"sqlite:///{DB_PATH}"
-
-engine = create_engine(
-    DB_URL,
-    connect_args={"check_same_thread": False},
-    echo=False,
-)
 
 
-def init_db() -> None:
-    DB_PATH.parent.mkdir(parents=True, exist_ok=True)
-    SQLModel.metadata.create_all(engine)
-    with engine.connect() as conn:
-        conn.exec_driver_sql("PRAGMA journal_mode=WAL;")
-        conn.exec_driver_sql("PRAGMA synchronous=NORMAL;")
+def read_user_config():
+    with open("configuration.yml", "r") as file:
+        return yaml.safe_load(file)
 
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://pneumonoultrafinalisekaiquestonlinefantasyoftheeastconiosis3.online"
-]
-
-
-# - Ensure DB exists on startup
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
-    init_db()
     yield
 
 
 app = FastAPI(
     lifespan=lifespan,
+    user_config=read_user_config(),
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=app.extra.get("user_config", {}).get("cors_origins", ""),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-class User(BaseModel):
-    name: str | None = None
+def user_config():
+    return read_user_config()
 
 
 @app.get("/api/user")
-async def get_user() -> User:
-    return User(name=getpass.getuser())
+async def get_user(config: dict[str, Any] = Depends(user_config)):
+    return config
 
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-@app.get("/api/something")
-async def get_something(session: Session = Depends(get_session)):
-    statement = select(Ejeweje)
-    results = session.exec(statement).all()
-    return results
 
 if __name__ == "__main__":
     import uvicorn
